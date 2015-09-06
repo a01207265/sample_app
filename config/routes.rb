6ce380c0d264406af8e3b9b5781d459d3871f6d5
Eduardo Cervantes <a01207265@gmail.com>
2015-09-05 21:33:52 -0500

    Add a static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_p_ages/home'
+
+  get 'static_p_ages/help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
