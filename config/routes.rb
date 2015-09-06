be0248a8201828391a44d189a901f2439fb1b0a7
Eduardo Cervantes <a01207265@gmail.com>
2015-09-06 01:36:28 -0500

    corrected names static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root 'static_p_ages#home'
+  root 'static_pages#home'
 
-  get 'static_p_ages/help'
+  get 'static_pages/help'
 
-  get 'static_p_ages/about'
+  get 'static_pages/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
